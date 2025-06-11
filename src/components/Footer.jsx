98f0875369eaecd3c5ec33e8c3174518cdcab088
Jooyoung Jeong <72073045+zzooyoung@users.noreply.github.com>
2025-06-11 11:46:39 +0900

    CI/CD : 첫 시도

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
       <footer className="footer">
         <div className="container">
           <div className="footer-content">
-            <p className="copyright">&copy; 2023 Stock4U (스톡포유). All rights reserved.</p>
+            <p className="copyright">&copy; 2025 Stock4U (스톡포유). 동아대학교 2025학년도 1학기 도전학기제 팀 INL All rights reserved.</p>
             <div className="footer-links">
               <a href="#" className="footer-link">
                 Terms
